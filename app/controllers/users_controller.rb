16f8c68d5a4aeb2d3df3f2c6666c7f00c4374825
MitsuyoshiToguri <seiun340066@gmail.com>
2014-12-08 12:31:12 -0500

    you can upload user profilr picture if you use jpeg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    p @user.valid?
+    p @user.errors
+    
     if @user.save
       auto_login(@user)
       redirect_to user_path(current_user)
@@ -26,6 +29,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:email,:picture,:password,:password_confirmation)
+    params.require(:user).permit(:first_name,:last_name,:email,:picture,:picture_cache,:password,:password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :password, length: {minimum: 3}
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
-  validates :email, uniqueness: true
+  # validates :password, length: {minimum: 3}
+  validates :password, confirmation: true, on: :create
+  # validates :password_confirmation, presence: true
+  # validates :email, uniqueness: true
 
   mount_uploader :picture, UserImageUploader
+
 end
+
+
+	
