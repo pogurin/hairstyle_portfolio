2a2ad5835fb75b8aac709d4af5332311178054cf
Amy Rudolph <amy.rudolph@live.co.uk>
2014-12-08 00:41:58 -0500

    associations saved.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  has_many :reviews
+  has_many :hairdressers, through: :reviews
+
   validates :password, length: {minimum: 3}
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
