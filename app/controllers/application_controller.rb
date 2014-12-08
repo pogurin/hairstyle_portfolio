49059e34123a7ae13f04f17bc970e3acf456dba0
Amy Rudolph <amy.rudolph@live.co.uk>
2014-12-08 18:33:11 -0500

    Integrate Devise with separate User and Hairdresser models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_hairdresser
-    @current_hairdresser ||= Hairdresser.find(session[:hairdresser_id]) if session[:hairdresser_id]
-  end
+  protected
 
-  helper_method :current_hairdresser
-
-  def ensure_logged_in
-    unless current_hairdresser
-      flash[:alert] = "Please log in"
-      redirect_to new_session_path
-    end
-  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name,:last_name]
+  end  
 end
