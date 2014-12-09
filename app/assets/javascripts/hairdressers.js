f84b922a8f7a661cf5bd7f74552892b9986b607b
Amy Rudolph <amy.rudolph@live.co.uk>
2014-12-09 10:08:51 -0500

    added review form errors with ajax

--- app/assets/javascripts/hairdressers.js
+++ app/assets/javascripts/hairdressers.js
@@ -1,10 +1,11 @@
 $(document).on('ready page:load', function() { //need 'ready page:load' and not .ready so that tourbolinks works and responds with ajax requests to the server
-  
+ 
   //incase the browser takes a while saving the comment we disable the button temorarily.
   $('#new_review').on('ajax:beforeSend', function() {
     $('input[type=submit]').val('Saving....').attr('disabled', 'disabled');
   });
 
+  //re-enable the submit button after saving
   $('#new_review').on('ajax:complete', function() {
     $('input[type=submit]').val('Create Review').removeAttr('disabled');
   });
