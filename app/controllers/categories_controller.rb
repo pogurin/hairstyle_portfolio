4b64cf14ce8f0785d46461a37796eb70b0a4a48f
GABRIEL ROBERTSON <gabriel.t.robertson@gmail.com>
2014-12-10 17:37:24 -0500

    Implemented picture/category tagging

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,8 +10,13 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-        @pictures = Picture.all
-
+    @pictures = Picture.all
+    @pictures_category=[]
+    @pictures.each do |picture|
+      if picture.categories.find_by_name(@category.name)
+        @pictures_category << picture
+      end
+    end
   end
 
   def create
