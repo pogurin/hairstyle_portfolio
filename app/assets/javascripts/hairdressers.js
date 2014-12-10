a07d2c59b52c47feb41f4357b28d2a3eb6f4b788
Amy Rudolph <amy.rudolph@live.co.uk>
2014-12-09 20:10:01 -0500

    started login functionality with ajax

--- app/assets/javascripts/hairdressers.js
+++ app/assets/javascripts/hairdressers.js
@@ -1,5 +1,12 @@
 $(document).on('ready page:load', function() { //need 'ready page:load' and not .ready so that tourbolinks works and responds with ajax requests to the server
- 
+ 	$('#user_log_in').click(function(event) {
+  	event.preventDeafault();
+		$("#login_user").show();
+		$("#user_log_in").hide()
+	});
+
+
+
   //incase the browser takes a while saving the comment we disable the button temorarily.
   $('#new_review').on('ajax:beforeSend', function() {
     $('input[type=submit]').val('Saving....').attr('disabled', 'disabled');
