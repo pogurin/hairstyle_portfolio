c5628e247f285b187e0251c751af8112d6d42a88
GABRIEL ROBERTSON <gabriel.t.robertson@gmail.com>
2014-12-11 14:46:22 -0500

    Modified arrangement of hairstylist page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141211192259) do
-
 
   create_table "categories", force: true do |t|
     t.string   "name"
