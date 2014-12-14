369766a71623b47f147b038c0c0cfabe3c7a9b33
MitsuyoshiToguri <seiun340066@gmail.com>
2014-12-14 13:19:43 -0500

    Hidden field was solution, now specific hairdresser can get email from customer

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -12,7 +12,7 @@ class InquiryController < ApplicationController
  
   def confirm
     # 入力値のチェック
-    @inquiry = Inquiry.new(params[:inquiry])
+    @inquiry = Inquiry.new(inquiry_params)
     @inquiry.name = current_user.first_name
     if @inquiry.valid?
       # OK。確認画面を表示
@@ -26,11 +26,18 @@ class InquiryController < ApplicationController
   def thanks
     # メール送信
   
-    @inquiry = Inquiry.new(params[:inquiry])
+    @inquiry = Inquiry.new(inquiry_params)
     @inquiry.name = current_user.first_name
+   
     InquiryMailer.received_email(@inquiry, current_user).deliver
     
     # 完了画面を表示
     render :action => 'thanks'
   end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :hairdresser_email, :message,:hairdresser_first_name)
+  end 
+
 end
