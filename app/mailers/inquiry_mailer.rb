4bf64deb1d72a23d36e7c075a58874da9cc0cb82
MitsuyoshiToguri <seiun340066@gmail.com>
2014-12-14 14:38:01 -0500

    Hairdresser can go to Accept booking page, from their email

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,10 +2,11 @@ class InquiryMailer < ActionMailer::Base
      # 送信元アドレス
   def received_email(inquiry, user)
     @inquiry = inquiry
-    @url = 'goodle.com'
+    @url = 'inquiry_accept_url'
     mail(from: user.email,
     	 to: @inquiry.hairdresser_email,
     	:subject => 'Thank you for your message!')
   end
  
 end
+
