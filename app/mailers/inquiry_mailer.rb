fdbc53dc4cb6dc38769192207e3e4c14a0b78d0f
MitsuyoshiToguri <seiun340066@gmail.com>
2014-12-13 22:01:01 -0500

    now you can get email from customer(customer have to type email)

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,11 @@
 class InquiryMailer < ActionMailer::Base
-  default from: "seiun340066@gmail.com"   # 送信元アドレス
+     # 送信元アドレス
   default to: "seiun340066@gmail.com"     # 送信先アドレス
  
   def received_email(inquiry)
     @inquiry = inquiry
-    mail(:subject => 'Thank you for your message!')
+    mail(from: inquiry.email,
+    	:subject => 'Thank you for your message!')
   end
  
 end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -5,4 +5,5 @@ class Inquiry
  
   validates :name, :presence => {:message => 'Please type your Name'}
   validates :email, :presence => {:message => 'Please type your email'}
+  validates :message, :presence => {:message => 'Please type detail '}
 end
