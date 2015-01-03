bdfa8aa22a0f0e4fa638539b5e608551bbf4e71a
MitsuyoshiToguri <seiun340066@gmail.com>
2015-01-03 18:54:40 -0500

    followed youtube rails cast revised

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,48 +1,30 @@
 class SearchesController < ApplicationController
-  before_action :set_search, only: [:show, :edit, :update, :destroy]
+  # before_action :set_search, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+  # respond_to :html
 
-  def indexlle
-    @searches = Search.all
-    respond_with(@searches)
-  end
 
   def show
-    respond_with(@search)
-    @hairdresser = Hairdresser.find(params[:id])
+    @search = Search.find(params[:id])
   end
 
   def new
     @search = Search.new
-    respond_with(@search)
-  end
-
-  def edit
   end
 
   def create
-    @search = Search.new(search_params)
-    @search.save
-    respond_with(@search)
+    @search = Search.create!(params[:search])
+    redirect_to @search 
   end
-
-  def update
-    @search.update(search_params)
-    respond_with(@search)
-  end
-
-  def destroy
-    @search.destroy
-    respond_with(@search)
-  end
-
-  private
-    def set_search
-      @search = Search.find(params[:id])
-    end
-
-    def search_params
-      params.require(:search).permit(:search,:area)
-    end
 end
+
+
+#   private
+#     def set_search
+#       @search = Search.find(params[:id])
+#     end
+
+#     def search_params
+#       params.require(:search).permit(:search,:area)
+#     end
+# end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,32 +1,13 @@
 class Search < ActiveRecord::Base
  
-	 def products
-	  @products ||= find_products
+	 def hairdressers
+	  @hairdressers ||= find_hairdressers
 	 end
 
 	private
 
-	def find_products
-	  Product.find(:all, :conditions => conditions)
-	end
-
-	def keyword_conditions
-	  ["products.name LIKE ?", "%#{keywords}%"] unless keywords.blank?
-	end
-
-	def conditions
-	  [conditions_clauses.join(' AND '), *conditions_options]
-	end
-
-	def conditions_clauses
-	  conditions_parts.map { |condition| condition.first }
-	end
-
-	def conditions_options
-	  conditions_parts.map { |condition| condition[1..-1] }.flatten
-	end
-
-	def conditions_parts
-	  private_methods(false).grep(/_conditions$/).map { |m| send(m) }.compact
+	def find_hairdressers
+		hairdressers = hairdresser.order(:name)
+		hairdressers = hairdresser.where("name like ?", "%#{keywords}%") if keywords.present?
 	end
 end
