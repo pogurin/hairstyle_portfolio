430b5c897f802b77775a1a730bd6d8eeaba2f8c5
GABRIEL ROBERTSON <gabriel.t.robertson@gmail.com>
2015-01-06 22:38:05 -0500

    Started work on search

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,6 +4,10 @@ class Search < ActiveRecord::Base
 	  @hairdressers ||= find_hairdressers
 	 end
 
+	 def pictures
+	 	@pictures ||= find_pictures
+	 end
+
 private
 
 	def find_hairdressers
@@ -14,36 +18,12 @@ private
    		hairdressers = hairdressers.where("style like ?", "%#{style}%") if style.present?
    		hairdressers = hairdressers.where("area like ?", "%#{area}%") if area.present?
    		hairdressers
-
-		  # Hairdresser.find(:all, :conditions => conditions)
-
-		# hairdressers = Hairdresser.order(:first_name)
-		# hairdressers = hairdressers.where("name like ?", "%#{first_name}%") if first_name.present?
-		# hairdressers = hairdressers.where("area like ?", "%#{area}%") if area.present?
-		# hairdressers = hairdressers.where(hairdresser_id: hairdresser_id) if hairdresser_id.present?
 	end
 
-	# def keyword_conditions
-	#   ["hairdressers.area LIKE ?", "%#{area}%"] unless area.blank?
-	# end	
+	def find_pictures
+		pictures = Picture.order(:category_id)
+		pictures = pictures.where(category_id: category_id) if category_id.present?
+		pictures
+	end
 
-	# def category_conditions
-	#   ["hairdressers.hairdresser_id = ?", hairdresser_id] unless hairdresser_id.blank?
-	# end	
-
-	# def conditions
-	#   [conditions_clauses.join(' AND '), *conditions_options]
-	# end
-
-	# def conditions_clauses
-	#   conditions_parts.map { |condition| condition.first }
-	# end
-
-	# def conditions_options
-	#   conditions_parts.map { |condition| condition[1..-1] }.flatten
-	# end
-
-	# def conditions_parts
-	#   private_methods(false).grep(/_conditions$/).map { |m| send(m) }.compact
-	# end	
 end
