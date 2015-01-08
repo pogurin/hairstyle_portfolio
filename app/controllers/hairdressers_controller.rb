ed158f012b7629b38d9ee985973562e030bed0af
GABRIEL ROBERTSON <gabriel.t.robertson@gmail.com>
2015-01-08 14:49:47 -0500

    Adjusted fields

--- app/controllers/hairdressers_controller.rb
+++ app/controllers/hairdressers_controller.rb
@@ -58,6 +58,7 @@ respond_to :html, :json
     else
       format.html { render :action => "edit" }
     end
+    
   #   if hairdresser_params != nil
   #   @hairdresser.update_attributes(hairdresser_params)
   # end
