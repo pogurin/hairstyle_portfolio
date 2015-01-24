d38a379c662a9e4d99a4c2b14794f628eacb573d
MitsuyoshiToguri <seiun340066@gmail.com>
2015-01-24 15:25:40 -0500

    port 25

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.gmail.com',
-    :port => '587',
-    # :domain => 'smtp.gmail.com',
+    :port => '25',
+    :domain => 'smtp.gmail.com',
     :authentication => 'plain',
     :user_name => 'seiun340066@gmail.com',
     :password => 'seiun340066'
