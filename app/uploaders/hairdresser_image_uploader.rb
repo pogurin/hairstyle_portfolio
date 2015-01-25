7efbff24df45479676b336bf78342a5e56fca8ff
MitsuyoshiToguri <seiun340066@gmail.com>
2015-01-25 02:09:04 -0500

    uploader

--- app/uploaders/hairdresser_image_uploader.rb
+++ app/uploaders/hairdresser_image_uploader.rb
@@ -7,8 +7,8 @@ class HairdresserImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
-  # storage :fog
+  # storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
