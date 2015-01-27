50665ae20cf11a0701436f40a061a8d21a5a22c7
GABRIEL ROBERTSON <gabriel.t.robertson@gmail.com>
2015-01-27 17:29:25 -0500

    Changed seeds file

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -9,5 +9,6 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = 'airsalon'                          # required
   # config.fog_public     = false                                        # optional, defaults to true
-  # config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+  # config.fog_attributes =
+   {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
 end
